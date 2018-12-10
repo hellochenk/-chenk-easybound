4c856d636857223e2fe56f2588e44818d6f750b0
hellochenk <657301050@qq.com>
2018-12-10 13:51:35 +0800

    add css module

--- src/webpack-module.js
+++ src/webpack-module.js
@@ -43,7 +43,10 @@ export default class Module extends Configure {
 					loader: "style-loader" // 将 JS 字符串生成为 style 节点
 				},
 				{
-					loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+					loader: "css-loader", // 将 CSS 转化成 CommonJS 模块
+					options: {
+						modules: true
+					}
 				},
 				{
 					loader: "sass-loader" // 将 Sass 编译成 CSS
